5bb607654b1a836323fed2209cec5b9e4432027d
Mark Villar <mark.villar@myport.ac.uk>
2019-12-18 20:41:48 +0000

    Compose Tweet

---
+++ Twitter/Controller/ComposeTweetController.swift
@@ -0,0 +1,47 @@
+//
+//  ComposeTweetController.swift
+//  Twitter
+//
+//  Created by Mark on 18/12/2019.
+//  Copyright © 2019 Twitter. All rights reserved.
+//
+
+import UIKit
+
+class ComposeTweetController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigation()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view = ComposeTweetView()
+    }
+    
+    fileprivate func setupNavigation() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(composeTweet))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissComposeTweet))
+    }
+    
+    @objc func composeTweet() {
+        
+        guard let composeView = view as? ComposeTweetView else { dismissComposeTweet(); return }
+        
+        guard let tweet = composeView.tweetText.text else { return }
+        
+        let composedTweet = Tweet(content: tweet, user: User(firstName: "Mark", lastName: "Villar", userName: "@mark"))
+        
+        let firestore = NetworkCall()
+        firestore.add(data: composedTweet, in: Subcollections.tweets)
+        
+        dismissComposeTweet()
+    }
+    
+    @objc func dismissComposeTweet() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Twitter/View/ComposeTweetView.swift
@@ -0,0 +1,46 @@
+//
+//  ComposeTweetView.swift
+//  Twitter
+//
+//  Created by Mark on 18/12/2019.
+//  Copyright © 2019 Twitter. All rights reserved.
+//
+
+import UIKit
+
+class ComposeTweetView: UIView {
+    
+    let tweetText: UITextView = {
+        let textview = UITextView()
+        textview.isScrollEnabled = false
+        textview.textAlignment = .left
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        return textview
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupConstraints() {
+        addSubview(tweetText)
+        
+        NSLayoutConstraint.activate([
+            tweetText.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tweetText.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            tweetText.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            tweetText.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    fileprivate func setupView() {
+        backgroundColor = .white
+        setupConstraints()
+    }
+    
+}

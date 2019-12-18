7b1495406e600272ae64e217717fdb7d2615cbad
Mark Villar <mark.villar@myport.ac.uk>
2019-12-18 20:42:25 +0000

    static subcollections

---
+++ Twitter/Utilities/Subcollections.swift
@@ -0,0 +1,14 @@
+//
+//  Subcollections.swift
+//  Twitter
+//
+//  Created by Mark on 18/12/2019.
+//  Copyright © 2019 Twitter. All rights reserved.
+//
+
+import Foundation
+
+struct Subcollections {
+    static let tweets = "tweets"
+    static let users = "users"
+}
